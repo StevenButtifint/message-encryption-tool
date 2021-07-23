6f39ab68f1a4e3672391500e243017ce2b564a54
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2021-07-23 20:15:35 +0100

    added title interface

--- app.py
+++ app.py
@@ -8,12 +8,29 @@ APP_ICON    = "icon.ico"
 WINDOW_H    = 500
 WINDOW_W    = 500
 
-FIRST_COL   = "cyan4"
-SECOND_COL  = "cyan2"
+COL_PRIME   = "cyan4"
+COL_SECND   = "cyan2"
+COL_THIRD   = "DarkSlateGray3"
+COL_TEXT    = "white"
+
+
+def makeFrame(root):
+    return tk.Frame(root, bg=COL_PRIME)
+
+
+def makeLabel(frame, text, font_size):
+    return tk.Label(frame, text=text, bg=COL_PRIME, fg=COL_SECND, font=(COL_SECND,font_size))
+
+
+def createTitleInterface():
+    title_frame = makeFrame(root).place(relwidth=1, relheight=0.06, relx=0, rely=0)
+    title_label = makeLabel(title_frame, "Message Encrypter", 14).place(x=170, y=2)
+
+
 
 
 def createInterface():
-    pass
+    createTitleInterface()
 
 
 
@@ -23,5 +40,5 @@ if __name__ == "__main__":
     root.resizable(width=False, height=False)
     root.title(APP_TITLE)
     root.iconbitmap(APP_ICON)
-    canvas = tk.Canvas(root, height=WINDOW_H, width=WINDOW_W, bg=FIRST_COL).pack()
+    canvas = tk.Canvas(root, height=WINDOW_H, width=WINDOW_W, bg=COL_THIRD).pack()
     createInterface()
