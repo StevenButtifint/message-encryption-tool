902f1701253c3497dfb3577c790902c4636ae7b1
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2021-11-25 23:07:45 +0000

    added encryption method

--- res/methods/CaesarCipher.py
+++ res/methods/CaesarCipher.py
@@ -7,3 +7,12 @@ class CaesarCipher(object):
         self.alphabet = "abcdefghijklmnopqrstuvwxyz"
         self.alphabet_len = len(self.alphabet)
 
+    def encrypt(self, plaintext):
+        ciphertext = ""
+        self._setShift()
+        for letter in plaintext:
+            index = self.alphabet.find(letter)
+            if index != -1:
+                ciphertext += self.alphabet[(index + self.shift) % self.alphabet_len]
+        return ciphertext
+
