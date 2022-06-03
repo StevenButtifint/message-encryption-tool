6a7cb48ff9c1cefb4dd0d402a83728878d6cbf9f
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2022-06-03 13:16:04 +0100

    added label constants

--- constants.py
+++ constants.py
@@ -7,6 +7,11 @@ COLOUR_THIRD = "DarkSlateGray3"
 COLOUR_TEXT = "black"
 
 
+#labels
+CRYPTO_METHODS = ["Caesar Cipher", "Vernam Cipher", "AES-128 Symmetric-Key", "AES-256 Custom-Key", "RSA-512 Asymmetric-Key"]
+OPERATIONS = ["Encrypt", "Decrypt"]
+
+
 #window
 RESOLUTION = "500x500"
 TITLE = "Message Encryption Tool"
