8f9c911ded940d26ea21ddaa298ad96585707b0b
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2022-06-03 13:15:51 +0100

    added window constants

--- constants.py
+++ constants.py
@@ -5,3 +5,9 @@ COLOUR_PRIME = "cyan4"
 COLOUR_SECOND = "cyan2"
 COLOUR_THIRD = "DarkSlateGray3"
 COLOUR_TEXT = "black"
+
+
+#window
+RESOLUTION = "500x500"
+TITLE = "Message Encryption Tool"
+ICON_DIR = "res/icons/icon.ico"
