1a09e805fd5ac624546c2215f10422aa2c86797f
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2022-06-19 19:07:22 +0100

    added method object dictionary

--- constants.py
+++ constants.py
@@ -1,3 +1,8 @@
+from cryptographic_methods.AES_128 import AES_128
+from cryptographic_methods.RSA_512 import RSA_512
+from cryptographic_methods.caesar_cipher import CaesarCipher
+from cryptographic_methods.vernam_cipher import VernamCipher
+from cryptographic_methods.AES_256_custom_key import AES_256_custom_key
 
 
 #colours
@@ -16,3 +21,9 @@ OPERATIONS = ["Encrypt", "Decrypt"]
 RESOLUTION = "500x500"
 TITLE = "Message Encryption Tool"
 ICON_DIR = "res/icons/icon.ico"
+
+
+METHOD_DICT = {
+    "Caesar Cipher": CaesarCipher,
+    "Vernam Cipher": VernamCipher}
+    
