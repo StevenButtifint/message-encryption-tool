ad972b45ff3ad9f34dd096a0a474deccf7962335
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2022-11-01 16:28:24 +0000

    added make entry interface function

--- interface.py
+++ interface.py
@@ -30,6 +30,12 @@ def makeLabel(frame, text, font_size, x, y, anchor, bg):
     return label
 
 
+def makeEntry(frame, width, bg, fg, rx, ry):
+    entry = tk.Entry(frame, width=width, bg=bg, fg=fg)
+    entry.place(relx=rx, rely=ry)
+    return entry
+
+
 def makeTextbox(frame, width, height, bg, fg, rx, ry):
     textbox = tk.Text(frame, width=width, height=height, bg=bg, fg=fg)
     textbox.place(relx=rx, rely=ry, anchor="center")
