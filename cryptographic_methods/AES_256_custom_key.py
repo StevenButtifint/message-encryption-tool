906c5298ca4d298c74491159c080fa9ccce84bd9
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2022-11-16 19:13:09 +0000

    added AES custom encryption

--- cryptographic_methods/AES_256_custom_key.py
+++ cryptographic_methods/AES_256_custom_key.py
@@ -8,3 +8,10 @@ class AES_256_custom_key(object):
     def __init__(self):
         self.block_size = AES.block_size
         
+    def encrypt(self, plaintext, key):
+        key = hashlib.sha256(key.encode()).digest()
+        iv = Random.new().read(AES.block_size)
+        plaintext = self._pad(plaintext)
+        ciphertext = AES.new(key, AES.MODE_CBC, iv)
+        return self._encode_base_64(iv + ciphertext.encrypt(plaintext.encode()))
+
