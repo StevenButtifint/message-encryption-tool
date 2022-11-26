03353b87243f2e40485fa6b2ef5e7670f9f7beb7
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2022-11-26 10:48:30 +0000

    unified spacing

--- cryptographic_methods/caesar_cipher.py
+++ cryptographic_methods/caesar_cipher.py
@@ -1,11 +1,11 @@
 from random import randint
 
+
 class CaesarCipher(object):
     def __init__(self, shift):
         self.shift = shift
         self.alphabet = "abcdefghijklmnopqrstuvwxyz"
         self.alphabet_len = len(self.alphabet)
-
 
     def encrypt(self, plaintext):
         ciphertext = ""
@@ -15,7 +15,6 @@ class CaesarCipher(object):
             if index != -1:
                 ciphertext += self.alphabet[(index + self.shift) % self.alphabet_len]
         return ciphertext
-
 
     def decrypt(self, ciphertext):
         plaintext = ""
@@ -29,19 +28,8 @@ class CaesarCipher(object):
                 pass
         return plaintext
 
-        
     def _setShift(self):
         if self.shift == "":
             self.shift = randint(2, self.alphabet_len)
         else:
             self.shift = int(self.shift)
-
-
-    def sanitizeEncrypt(self, plaintext):
-        ciphertext = self.encrypt(plaintext)
-        return ("Shift:\n" + str(self.shift) +"\n\n" + "Ciphertext:\n" + str(ciphertext))
-
-
-    def sanitizeDecrypt(self, ciphertext):
-        plaintext = self.decrypt(ciphertext)
-        return ("Shift:\n" + str(self.shift) +"\n\n" + "Plaintext:\n" + str(plaintext))
