93c848da62be1c82df5879d0a972abf13eed28e1
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2022-11-28 19:49:33 +0000

    added RSA encryption method

--- cryptographic_methods/RSA.py
+++ cryptographic_methods/RSA.py
@@ -1,6 +1,18 @@
 
+from Crypto.PublicKey import RSA as _RSA
+from Crypto.Cipher import PKCS1_OAEP
+import base64
+
 
 class RSA:
     def __init__(self, key_size):
         self.key_size = key_size
+
+    def encrypt(self, plaintext):
+        private_key = _RSA.generate(self.key_size)
+        public_key = private_key.publickey()
+        encryption = PKCS1_OAEP.new(private_key)
+        ciphertext = encryption.encrypt(bytes(plaintext.encode('utf-8')))
+        encoded_ciphertext = base64.b64encode(ciphertext)
+        return encoded_ciphertext, private_key.exportKey("PEM"), public_key.exportKey("PEM")
 
